374c16a4df3f2ba559621f4dc8abe9b78e5d2d7e
Preeti Pal <palpreeti2245@gmail.com>
2026-09-03 21:43:22 +0530

    SQL formate
    (ctrl k):- SQL Formate

---
+++ SQL Formate.sql
@@ -0,0 +1,48 @@
+
+drop table if exists users;
+
+Create table users(
+		user_id int primary key,
+		username varchar(100) not null,
+		email varchar(100) unique,
+		age int,
+		city varchar(100) default('delhi')
+);
+
+	INSERT INTO users (user_id, username, email, age, city)
+	VALUES
+	(1, 'rahul123', 'rahul@gmail.com', 25, 'Mumbai'),
+	(2, 'priya_s', 'priya@gmail.com', 28, 'Delhi'),
+	(3, 'amit_kumar', 'amit@gmail.com', 30, 'Bangalore'),
+	(4, 'neha_verma', 'neha@gmail.com', 24, 'Pune'),
+	(5, 'rohan_g', 'rohan@gmail.com', 27, 'Chennai'),
+	(6, 'anjali_m', 'anjali@gmail.com', 26, 'Noida'),
+	(7, 'vikas_y', 'vikas@gmail.com', 32, 'Gurgaon'),
+	(8, 'sneha_p', 'sneha@gmail.com', 23, 'Kolkata'),
+	(9, 'arjun_r', 'arjun@gmail.com', 29, 'Hyderabad'),
+	(10, 'kavita_j', 'kavita@gmail.com', 31, 'Delhi');
+
+SELECT
+	*
+FROM
+	USERS
+ORDER BY
+	USER_ID DESC;
+UPDATE USERS
+SET
+	AGE = 30
+WHERE
+	USERNAME = 'vikas_y';
+
+UPDATE USERS
+SET
+	CITY = 'Delhi',
+	AGE = 30
+WHERE
+	USERNAME = 'arjun_r';
+
+UPDATE USERS
+SET
+	AGE = AGE + 1
+WHERE
+	EMAIL LIKE '%@gmail.com';
