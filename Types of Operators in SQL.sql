fd5d15a4d9c0bf106714a5e7479bd2f993c63fbe
Preeti Pal <palpreeti2245@gmail.com>
2026-09-06 11:44:21 +0530

    Types of Operators in  SQL

---
+++ Types of Operators in SQL.sql
@@ -0,0 +1,9 @@
+-- -- Types of Operaters in SQL 
+-- 1. Arithametic Operaters 
+-- 2. Comparison Operaters
+-- 3. Logical Opreaters 
+-- 4. Bitwise Operaters
+-- 5. Set Operaters
+-- 6. string Operaters
+-- 7. Other Oparaters
+
